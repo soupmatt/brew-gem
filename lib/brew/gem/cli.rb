f9be7c3bddfb3e393e559eeb57fe74125a08d199
Nick Sieger <nick@nicksieger.com>
2016-05-16 17:28:43 -0500

    Break up Brew::Gem::CLI.run into smaller pieces

--- lib/brew/gem/cli.rb
+++ lib/brew/gem/cli.rb
@@ -2,20 +2,28 @@ require 'erb'
 require 'tempfile'
 
 module Brew::Gem::CLI
-  HELP_MSG = <<-STR
-Please specify a gem name (e.g. brew gem command <name>)
-  install - Install a brew gem, accepts an optional version argument (e.g. brew gem install <name> <version>)
-  upgrade - Upgrade to the latest version of a brew gem
-  uninstall - Uninstall a brew gem
-STR
+  module_function
 
-  def self.run(args = ARGV)
+  COMMANDS = {
+    "install"   => ("Install a brew gem, accepts an optional version argument\n" +
+                    "            (e.g. brew gem install <name> [version])"),
+    "upgrade"   => "Upgrade to the latest version of a brew gem",
+    "uninstall" => "Uninstall a brew gem",
+    "help"      => "This message"
+  }
+
+  def help_msg
+    (["Please specify a gem name (e.g. brew gem command <name>)"] +
+      COMMANDS.map {|name, desc| "  #{name} - #{desc}"}).join("\n")
+  end
+
+  def process_args(args)
     if !args[0] || args[0] == 'help'
-      abort HELP_MSG
+      abort help_msg
     end
 
-    command = args[0]
-    name = args[1]
+    command, name = args[0..1]
+
     gems = `gem list --remote "^#{name}$"`.lines
 
     unless gems.detect { |f| f =~ /^#{name} \(([^\s,]+).*\)/ }
@@ -24,20 +32,34 @@ STR
 
     version = args[2] || $1
 
+    [command, name, version]
+  end
+
+  def expand_template(name, version)
     klass         = 'Gem' + name.capitalize.gsub(/[-_.\s]([a-zA-Z0-9])/) { $1.upcase }.gsub('+', 'x')
     user_gemrc    = "#{ENV['HOME']}/.gemrc"
     template_file = File.expand_path('../template.rb.erb', __FILE__)
     template      = ERB.new(File.read(template_file))
-    filename      = File.join Dir.tmpdir, "gem-#{name}.rb"
+    template.result(binding)
+  end
 
-    begin
-      open(filename, 'w') do |f|
-        f.puts template.result(binding)
-      end
+  def with_temp_formula(name, version)
+    filename = File.join Dir.tmpdir, "gem-#{name}.rb"
 
+    open(filename, 'w') do |f|
+      f.puts expand_template(name, version)
+    end
+
+    yield filename
+  ensure
+    File.unlink filename
+  end
+
+  def run(args = ARGV)
+    command, name, version = process_args(args)
+
+    with_temp_formula(name, version) do |filename|
       system "brew #{command} #{filename}"
-    ensure
-      File.unlink filename
     end
   end
 end
