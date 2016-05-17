8371db6c1135ef59668df2f278e34efb219f83a4
Nick Sieger <nick@nicksieger.com>
2016-05-17 09:31:32 -0500

    Exclude integration test when `brew` command not found

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,3 +8,11 @@ else
   ::Dir.glob(::File.expand_path('../support/*.rb', __FILE__)).each { |f| require_relative f }
   ::Dir.glob(::File.expand_path('../support/**/*.rb', __FILE__)).each { |f| require_relative f }
 end
+
+RSpec.configure do |c|
+  c.filter_run focus: true
+  c.run_all_when_everything_filtered = true
+  unless system('which brew > /dev/null')
+    c.filter_run_excluding integration: true
+  end
+end
