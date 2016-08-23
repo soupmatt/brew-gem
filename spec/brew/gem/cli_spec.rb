cb794b35a47e4f5a5aa656db7047834e3503418f
Andy Fleener <andrew.fleener@sportngin.com>
2016-08-23 14:04:58 -0500

    Setup for the new use_homebrew_ruby options

--- spec/brew/gem/cli_spec.rb
+++ spec/brew/gem/cli_spec.rb
@@ -2,16 +2,23 @@ require 'spec_helper'
 
 RSpec.describe Brew::Gem::CLI do
   context "#expand_formula" do
-    subject(:formula) { described_class.expand_formula("foo-bar", "1.2.3") }
+    subject(:formula) { described_class.expand_formula("foo-bar", "1.2.3", false) }
 
     it "generates valid Ruby" do
       IO.popen("ruby -c -", "r+") { |f| f.puts formula }
+
       expect($?).to be_success
     end
 
     it { is_expected.to match(/class GemFooBar < Formula/) }
 
     it { is_expected.to match(/version "1\.2\.3"/) }
+    it { is_expected.to match("rubybindir = '/usr/bin'") }
+
+    context "homebrew-ruby" do
+      subject(:formula) { described_class.expand_formula("foo-bar", "1.2.3", true) }
+      it { is_expected.to match("rubybindir = '/usr/local/bin'") }
+    end
   end
 
 end
